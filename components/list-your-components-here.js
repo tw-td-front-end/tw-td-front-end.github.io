bf5a042866c6456ac3e9224d7122546771b2a83c
Ruigang Sun <ruigang.sun@CNruigang.sun>
2019-08-11 12:28:04 +0800

    initialize the components and problems

---
+++ components/list-your-components-here.js
@@ -0,0 +1,9 @@
+const allComponents = [
+  {
+    componentName: "",
+    framework: "", // vue,react,angular,original
+    occurredDate: "2019/08/11",
+    author: "", // may be "bob,jack"
+  },
+  // need your contribution
+];
---
+++ problems/list-your-problems.js
@@ -0,0 +1,8 @@
+const allProblems = [
+  {
+    problem: "",
+    occurredDate: "2019/08/11", // solved date
+    author: "", // who solve it, the may be "bob,jack"
+  },
+  // need your contribution
+];
