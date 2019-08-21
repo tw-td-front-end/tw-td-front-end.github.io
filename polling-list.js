9ed4778c5375e8aeb14474edd851df24285906db
Ruigang Sun <ruigang.sun@CNruigang.sun>
2019-08-21 20:31:03 +0800

    upload the first session and add polling list

---
+++ polling-list.js
@@ -0,0 +1,32 @@
+var pollingList = [
+  {
+    greatDeveloper: '孙瑞刚',
+    times: 0 + 1, // 1 + 1
+  },
+  {
+    greatDeveloper: '李浩,闫双辉',
+    times: 0,
+  },
+];
+
+const nextCatchUp = {
+  host: '孙瑞刚',
+  date: '2019/08/23',
+};
+
+const getRankList = () => {
+  return pollingList.sort((per1, per2) => per2.times - per1.times).map(per => per.greatDeveloper);
+};
+
+const getLeastTimesPerson = () => {
+  let leastTimesPerson = pollingList[0];
+  pollingList.forEach((person) => {
+    if (person.times < leastTimesPerson.times) {
+      leastTimesPerson = person;
+    }
+  });
+  return leastTimesPerson;
+};
+
+console.log('Who has least times:', getLeastTimesPerson());
+console.log('Rank list:', getRankList());
--- sessions/list-your-session-here.js
+++ sessions/list-your-session-here.js
@@ -4,5 +4,10 @@ const allSession = [
     occurredDate: "2019/08/11",
     author: "", // may be "bob,jack"
   },
+  {
+    topic: "JavaScript精度问题",
+    occurredDate: "2019/08/23",
+    author: "孙瑞刚",
+  },
   // need your contribution
 ];
