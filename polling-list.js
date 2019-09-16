2d90f53e14b12cc218b9a9e07d3a437128fe26a1
Sun Ruigang <srg_simon@163.com>
2019-09-16 10:38:47 +0800

    update polling-list

--- polling-list.js
+++ polling-list.js
@@ -1,11 +1,11 @@
 var pollingList = [
   {
     greatDeveloper: '孙瑞刚',
-    times: 0 + 1, // 1 + 1
+    times: 1, // 1 + 1
   },
   {
     greatDeveloper: '李浩',
-    times: 0,
+    times: 1,
   },
   {
     greatDeveloper: '闫双辉',
@@ -30,13 +30,109 @@ var pollingList = [
   {
     greatDeveloper: '李登辉',
     times: 0,
+  },
+  {
+    greatDeveloper: '刘融',
+    times: 0,
+  },
+  {
+    greatDeveloper: '王西明',
+    times: 0,
+  },
+  {
+    greatDeveloper: '严腾',
+    times: 0,
+  },
+  {
+    greatDeveloper: '李毅平',
+    times: 0,
+  },
+  {
+    greatDeveloper: '张昊毅',
+    times: 0,
+  },
+  {
+    greatDeveloper: '王乾明',
+    times: 0,
+  },
+  {
+    greatDeveloper: '王霞',
+    times: 0,
+  },
+  {
+    greatDeveloper: '刘瑞',
+    times: 0,
+  },
+  {
+    greatDeveloper: '王吉',
+    times: 0,
+  },
+  {
+    greatDeveloper: '王尉',
+    times: 0,
+  },
+  {
+    greatDeveloper: '黄铎',
+    times: 0,
+  },
+  {
+    greatDeveloper: '王凡',
+    times: 0,
+  },
+  {
+    greatDeveloper: '杨勋',
+    times: 0,
+  },
+  {
+    greatDeveloper: '成建坤',
+    times: 0,
+  },
+  {
+    greatDeveloper: '王凯航',
+    times: 0,
+  },
+  {
+    greatDeveloper: '刘鑫',
+    times: 0,
+  },
+  {
+    greatDeveloper: '白利楠',
+    times: 0,
+  },
+  {
+    greatDeveloper: '骆承秀',
+    times: 0,
+  },
+  {
+    greatDeveloper: '廖雪青',
+    times: 0,
+  },
+  {
+    greatDeveloper: '郑任哲',
+    times: 0,
+  },
+  {
+    greatDeveloper: '贺倩茹',
+    times: 0,
+  },
+  {
+    greatDeveloper: '杜泽艳',
+    times: 0,
+  },
+  {
+    greatDeveloper: '王蕾',
+    times: 0,
+  },
+  {
+    greatDeveloper: '杨帆',
+    times: 0,
   }
 ];
 
 const nextCatchUp = [
   {
-    host: '孙瑞刚',
-    date: '2019/08/23',
+    host: '闫双辉',
+    date: '2019/09/20',
   },
 ];
 
